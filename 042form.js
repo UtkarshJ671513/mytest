2b9e9f4fdd10f673fcde8246357dc682ec5f852f
Utkarsh Jagzape <utkarshjagzape98@gmail.com>
2024-05-28 14:04:40 +0530

    042form.js created and commited

---
+++ 042form.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Login Page</title>
+   
+</head>
+<body>
+  
+    <form method="post">
+        <table>
+            
+            </tr>
+        </table> 
+        
+    </form>
+</body>
+</html>
